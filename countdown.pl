c22fac08901842111f29d22a5b4f0de40c075cb8
MPenevskoi <MPenevskoi@luxoft.com>
2017-04-13 10:35:32 +0300

    second

---
+++ countdown.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use IO::Handle;
+
+my ( $remaining, $total );
+
+$remaining = $total = shift(@ARGV);
+
+STDOUT->autoflush(1);
+
+while ( $remaining ) {
+    printf ( "Remaining %s/%s \r", $remaining--, $total );
+    sleep 1;
+}
+
+print "\n";
